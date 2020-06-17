9743395ebb7a710ed165a3002b6a35086fd275c3
Răzvan Ionescu <ionescu77@gmail.com>
2020-06-17 13:17:58 +0300

    Add admin to urls.py

--- src/DjangoRestApisPostgreSQL/urls.py
+++ src/DjangoRestApisPostgreSQL/urls.py
@@ -1,5 +1,8 @@
-from django.conf.urls import url, include 
- 
-urlpatterns = [ 
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
     url(r'^', include('tutorials.urls')),
 ]
